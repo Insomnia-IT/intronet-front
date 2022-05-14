0703af773fb7d35cd80baba7693167f9e9a8aca7
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2022-05-14 20:19:06 +0300

    Доделал апи объявлений

--- src/api/notes.ts
+++ src/api/notes.ts
@@ -1,7 +1,10 @@
-import { INotes } from './../stores/notes.store';
+import { INotes } from '../stores/notes/notes.store';
 import { AdminApi } from './admin';
+import { ALL_CATEGORY, ICategory } from './../stores/notes/categories.store';
+import { COUNT_NOTES_OF_PAGE } from 'src/stores';
 
 const notesRout = '/api/notes'
+const categoriesRout = 'categories'
 
 export default class NotesApi extends AdminApi {
   constructor() {
@@ -12,9 +15,18 @@ export default class NotesApi extends AdminApi {
     return this.fetch(`${notesRout}/${id}`)
   }
 
-  public getNotes(page?: number, count?: number): Promise<INotes[]> {
-    const params = (page ? `?Page=${page}` : '') + (count ? `&Count=${count}` : '')
-    return this.fetch(`${notesRout}/filter${params}`)
+  public getNotes(page = 1, count = COUNT_NOTES_OF_PAGE, categoriesIds: number[] = []): Promise<INotes[]> {
+    // Если в переданном categoriesIds есть общая категория,
+    // то нужно заменить массив с категориями на пустой массив,
+    // что бы в запросе не было категории, и сервер вернул их все.
+    let smartFilter = true
+    if (categoriesIds.includes(0)) {
+      categoriesIds = []
+      smartFilter = false
+    }
+
+    const url = `${notesRout}/filter?Page=${page}&Count=${count}&${categoriesIds.length > 0 ? (`CategoriesIds=` + categoriesIds.join('&CategoriesIds=')) : `IsSmartFilter=${smartFilter}`}`
+    return this.fetch(url)
   }
 
   public getNotesAll(): Promise<INotes[]> {
@@ -44,5 +56,32 @@ export default class NotesApi extends AdminApi {
 
   public deleteNote(id: number): Promise<null> {
     return this.adminFetch(`${notesRout}/delete/${id}`)
+  }
+
+  getCategory = (id: number): Promise<ICategory> => {
+    return this.fetch(`${notesRout}/category/${id}`)
+  }
+
+  getAllCategories = (): Promise<ICategory[]> => {
+    return this.fetch(`${notesRout}/${categoriesRout}`)
+  }
+
+  createNewCategory = (name: string): Promise<null> => {
+    return this.adminFetch(`${notesRout}/${categoriesRout}/add`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name
+      })
+    })
+  }
+
+  editCategory = (id: number, newName: string): Promise<null> => {
+    return this.adminFetch(`${notesRout}/${categoriesRout}/edit`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id,
+        name: newName
+      })
+    })
   }
 }
