775dd8c851cb7b712fdbbcd0f00e3a680e86f680
andrey <andrey-ne@mail.ru>
2022-05-16 10:49:12 +0700

    map-page component vs function

--- src/pages/map/map-page.tsx
+++ src/pages/map/map-page.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { MapComponent } from "./map";
-import { useCellState } from "../../helpers/cell-state";
+import { cellState, useCellState } from "../../helpers/cell-state";
 import { mapStore } from "src/stores/map.store";
 import { Button, Icon } from "react-bulma-components";
 import styles from "./map-page.module.css";
+import { Observable } from "cellx-decorators";
 
 export function MapPage() {
   const [isMap, setIsMap, isMapCell] = useCellState(true);
@@ -23,4 +24,29 @@ export function MapPage() {
       </div>
     </>
   );
+}
+
+export class MapPage2 extends React.PureComponent {
+  @Observable
+  isMap = true;
+
+  state = cellState(this, {
+    image: () => (this.isMap ? mapStore.Map : mapStore.Schema),
+  });
+
+  render() {
+    if (!this.state.image) return <></>;
+    return (
+      <>
+        <MapComponent items={[]} image={this.state.image} onSelect={null} />;
+        <div className={styles.layers}>
+          <Button onClick={() => (this.isMap = !this.isMap)}>
+            <Icon>
+              <i className="mdi mdi-layers"></i>
+            </Icon>
+          </Button>
+        </div>
+      </>
+    );
+  }
 }
