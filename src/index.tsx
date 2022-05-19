6529c46c6b2a3576ed5bc11f68dba994afcf42cf
andrey <andrey-ne@mail.ru>
2022-05-19 12:40:14 +0700

    upd version and fix index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import "./index.sass";
 import { App } from "./app/app";
-import { render } from "react-dom";
 
 const container = document.getElementById("root");
-render(
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  container
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
