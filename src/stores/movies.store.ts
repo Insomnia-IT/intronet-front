9116c37c1df7bdd48a725613425360c0a546284e
andrey <andrey-ne@mail.ru>
2022-05-24 13:51:06 +0700

    movies store and voting page start

---
+++ src/stores/movies.store.ts
@@ -0,0 +1,44 @@
+import { Observable } from "cellx-decorators";
+import { ObservableDB } from "./observableDB";
+
+class MoviesStore {
+  @Observable
+  private db = new ObservableDB<Movie>("movies");
+
+  public get Movies(): Movie[] {
+    return this.db.toArray();
+  }
+
+  constructor() {
+    this.db.isLoaded.then(() => {
+      this.load();
+    });
+  }
+
+  load() {
+    Promise.reject<Movie[]>("api not found")
+      .catch(
+        (x) =>
+          [
+            {
+              title: "Крестный отец",
+              author: "Скорсезе",
+              id: 1,
+            },
+            {
+              title: "Королевство полной луны",
+              author: "Андерсон",
+              id: 2,
+            },
+            {
+              title: "Титаник",
+              author: "Кэмерон",
+              id: 3,
+            },
+          ] as Movie[]
+      )
+      .then((x) => this.db.merge(x, "server"));
+  }
+}
+
+export const moviesStore = new MoviesStore();
