bed8c51c3e096b1bb890aa0b526c6ac39be3b5ec
Corvus278 <makar.korolenko2005@mail.ru>
2022-05-27 16:25:37 +0300

    Убрал скроллбар у категорий

--- src/pages/board/boardPage/categories/categories.tsx
+++ src/pages/board/boardPage/categories/categories.tsx
@@ -11,11 +11,6 @@ export interface ICategoriesProps {
   onChangeCategory: (id: number) => void
 }
 
-export interface ITab {
-  text: string
-  id: number
-}
-
 @Observer
 export default class Categories extends React.Component<{}, {}> {
 
@@ -27,7 +22,7 @@ export default class Categories extends React.Component<{}, {}> {
 
   render() {
     return (
-      <Box display={'flex'} flex={1} overflowX={'auto'}>
+      <Box display={'flex'} flex={1} overflowX={'auto'} className='hide-scrollbar'>
         <HStack spacing={2} as='ul' flex={1} minWidth={'max-content'}>
           <Loading isLoading={categoriesStore.isLoading} height={40} width={40}>
             {categoriesStore.allCategory.map(category => {
