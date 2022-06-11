6943b8625e1fd60ec349f213f8eee19e1b11fee0
fvrrrn <BBChernyshov-Zagaevskiy@sberbank.ru>
2022-06-11 19:42:55 +0300

    IN-F6 Fixed typo

--- src/pages/board/boardPage/boardList/boardCard/noteText/noteText.tsx
+++ src/pages/board/boardPage/boardList/boardCard/noteText/noteText.tsx
@@ -1,32 +1,36 @@
-import * as React from 'react'
-import { Text, TextProps } from '@chakra-ui/react';
-import { ToggleBtn } from './toggleBtn/toggleBtn';
+import * as React from "react";
+import { Text, TextProps } from "@chakra-ui/react";
+import { ToggleBtn } from "./toggleBtn/toggleBtn";
 
 export type INoteText = {
-  text: INotes['text']
-}
+  text: INote["text"];
+};
 
-const COUNT_CHAR_OF_SHORT_TEXT = 176
+const COUNT_CHAR_OF_SHORT_TEXT = 176;
 
-const WrapText = ({ children, ...res }: React.PropsWithChildren<TextProps>) => (<Text as={'p'} fontSize={'sm'} {...res}>{children}</Text>)
+const WrapText = ({ children, ...res }: React.PropsWithChildren<TextProps>) => (
+  <Text as={"p"} fontSize={"sm"} {...res}>
+    {children}
+  </Text>
+);
 
 export const NoteText = ({ text }: INoteText) => {
-  const [isOpen, setIsOpen] = React.useState(false)
-  const toggleOpen = () => setIsOpen(prev => !prev)
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleOpen = () => setIsOpen((prev) => !prev);
 
-  const isBig = text.length > COUNT_CHAR_OF_SHORT_TEXT
-  if (!isBig) return (<WrapText>{text}</WrapText>)
+  const isBig = text.length > COUNT_CHAR_OF_SHORT_TEXT;
+  if (!isBig) return <WrapText>{text}</WrapText>;
 
-  const ending = '...'
-  const shortText = text.slice(0, COUNT_CHAR_OF_SHORT_TEXT - ending.length) + ending
+  const ending = "...";
+  const shortText =
+    text.slice(0, COUNT_CHAR_OF_SHORT_TEXT - ending.length) + ending;
 
   return (
     <WrapText>
-      {!isOpen ? shortText : text}
-      {' '}
+      {!isOpen ? shortText : text}{" "}
       <ToggleBtn onClick={toggleOpen}>
-        {isOpen ? 'Свернуть' : 'Подробнее'}
+        {isOpen ? "Свернуть" : "Подробнее"}
       </ToggleBtn>
     </WrapText>
-  )
-}
+  );
+};
