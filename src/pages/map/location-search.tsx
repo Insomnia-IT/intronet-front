443159eb933cde287e2637e3c956eab9f7607697
andrey <andrey-ne@mail.ru>
2022-06-15 10:19:30 +0400

    search tags and clean styles

--- src/pages/map/location-search.tsx
+++ src/pages/map/location-search.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { CloseIcon, SearchIcon } from "@chakra-ui/icons";
 import { cellState } from "src/helpers/cell-state";
 import { locationsStore } from "../../stores/locations.store";
 import { Observable } from "cellx-decorators";
 import { MapIcons } from "./icons/icons";
 import styles from "./map-page.module.css";
+import { Chip } from "../../components/chip/chip";
+import { ObservableList } from "cellx-collections";
+import { Close } from "src/components/close";
 
 export class LocationSearch extends React.PureComponent<{
   onSelect(location: InsomniaLocation);
 }> {
+  @Observable
+  choosedTags = new ObservableList<Tag>();
+
   @Observable
   query: string;
 
@@ -19,8 +32,14 @@ export class LocationSearch extends React.PureComponent<{
   state = cellState(this, {
     opened: () => this.opened,
     query: () => this.query,
+    tags: () => locationsStore.Tags.toArray(),
+    choosedTags: () => this.choosedTags.toArray(),
     locations: () =>
-      locationsStore.FullLocations.filter(filterLocations(this.query)),
+      locationsStore.FullLocations.filter(
+        (x) =>
+          this.choosedTags.length == 0 ||
+          x.tags.some((tag) => this.choosedTags.some((y) => y.id == tag.id))
+      ).filter(filterLocations(this.query)),
   });
 
   root = React.createRef<HTMLDivElement>();
@@ -31,19 +50,61 @@ export class LocationSearch extends React.PureComponent<{
         ref={this.root}
         className={this.state.opened ? styles.searchOpened : styles.search}
       >
-        <InputGroup>
-          <InputLeftElement
-            pointerEvents="none"
-            children={<SearchIcon color="gray.300" />}
-          />
-          <Input bg={"white"} onChange={(e) => (this.query = e.target.value)} />
-        </InputGroup>
+        <Flex direction="row">
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon color="gray.300" />
+            </InputLeftElement>
+            <Input
+              bg={"white"}
+              value={this.state.query}
+              placeholder="Поиск мест и мероприятий"
+              onChange={(e) => (this.query = e.target.value)}
+            />
+            {this.state.query && (
+              <InputRightElement onClick={() => (this.query = "")}>
+                <CloseIcon color="gray.200" />
+              </InputRightElement>
+            )}
+          </InputGroup>
+          <div className={styles.close} onClick={() => (this.opened = false)}>
+            <Close />
+          </div>
+        </Flex>
         {this.state.opened && (
           <>
+            <HStack
+              className={styles.chips}
+              padding="16px 0"
+              align="center"
+              flexDirection="row"
+              overflowX="scroll"
+            >
+              {this.state.tags.map((tag) => {
+                return (
+                  <Chip
+                    key={tag.id}
+                    className={styles.chip}
+                    activeClassName={styles.chipActive}
+                    onClick={() => {
+                      this.choosedTags.contains(tag)
+                        ? this.choosedTags.remove(tag)
+                        : this.choosedTags.add(tag);
+                    }}
+                    active={this.state.choosedTags.includes(tag)}
+                  >
+                    {tag.name}
+                  </Chip>
+                );
+              })}
+            </HStack>
             <Flex direction="column" bg="white">
               {this.state.locations.map((x) => (
                 <Flex
                   key={x.id}
+                  padding="16px"
+                  gap="10px"
+                  alignItems="flex-end"
                   onClick={() => {
                     this.props.onSelect(x);
                     this.opened = false;
@@ -85,7 +146,7 @@ export class LocationSearch extends React.PureComponent<{
 }
 
 function filterLocations(query: string) {
-  const regEx = new RegExp(query);
+  const regEx = new RegExp(query, "iu");
   return (location: InsomniaLocationFull) => {
     return location.name.match(regEx);
   };
