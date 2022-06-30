fcfe32f6c6fcc6ea28fb1949183d5dea6845433f
Corvus278 <makar.korolenko2005@mail.ru>
2022-06-30 19:12:40 +0300

    Сделал рандомный заголовок на главной

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -3,23 +3,34 @@ import * as React from "react";
 import { Heading } from "../../components/heading/heading";
 import { LoginButton } from "../../components/loginButton/loginButton";
 import { CardList } from "./cardList/cardList";
+import { getRandomItem } from "../../helpers/getRandomItem";
+import { titlesList } from "./titelsList";
 
 export const MainPage = () => {
   return (
-    <Container h={"100%"} overflowY={"auto"} className={"hide-scrollbar"}>
-      <VStack spacing={4} align={"flex-start"} w={"100%"} maxH={"100%"} pt={16}>
-        <Box
-          pb={5}
-          display={"flex"}
+    <>
+      {/* Login button - кнопка для админов, невидимая, располагается в правом верхнем углу экрана */}
+      <LoginButton position={"absolute"} top={0} right={0} opacity={0} />
+      <Container h={"100%"} overflowY={"auto"} className={"hide-scrollbar"}>
+        <VStack
+          spacing={4}
+          align={"flex-start"}
           w={"100%"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
+          maxH={"100%"}
+          pt={16}
         >
-          <Heading level={1}>Главная</Heading>
-          <LoginButton />
-        </Box>
-        <CardList></CardList>
-      </VStack>
-    </Container>
+          <Box
+            pb={5}
+            display={"flex"}
+            w={"100%"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Heading level={1}>{getRandomItem(titlesList)}</Heading>
+          </Box>
+          <CardList></CardList>
+        </VStack>
+      </Container>
+    </>
   );
 };
