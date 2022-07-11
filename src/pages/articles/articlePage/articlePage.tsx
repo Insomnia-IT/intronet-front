1fc313d2a111fc8025cd4e3fdd6188f3a65d5896
andrey <andrey-ne@mail.ru>
2022-07-11 16:02:04 +0400

    fix article loading

--- src/pages/articles/articlePage/articlePage.tsx
+++ src/pages/articles/articlePage/articlePage.tsx
@@ -1,4 +1,4 @@
-import { Container, VStack, Text } from "@chakra-ui/react";
+import { Container, Text, VStack } from "@chakra-ui/react";
 import { Observer } from "cellx-react";
 import * as React from "react";
 import { BackRoutButton } from "src/components/backRoutButton/backRoutButton";
@@ -8,33 +8,60 @@ import Loading from "src/loading/loading";
 import { articlesStore } from "../../../stores/articles.store";
 import { Article } from "./article/article";
 
-@Observer
 export class ArticlePage extends React.Component<WithIdProps> {
   componentDidMount(): void {
-    articlesStore.load();
+    this.updateArticle();
   }
+
+  componentDidUpdate(
+    prevProps: Readonly<WithIdProps>,
+    prevState: Readonly<{}>,
+    snapshot?: any
+  ) {
+    if (prevProps.id !== this.props.id) {
+      this.updateArticle();
+    }
+  }
+
+  async updateArticle() {
+    const id = +this.props.id;
+    let existed = articlesStore.getArticle(id);
+    if (!existed) {
+      this.setState({
+        IsLoading: true,
+      });
+      await articlesStore.load();
+      existed = articlesStore.getArticle(id);
+    }
+    this.setState({
+      article: existed,
+      IsLoading: false,
+    });
+  }
+
+  state = {
+    IsLoading: true,
+    article: null,
+  };
 
   render(): React.ReactNode {
     return (
       <VStack pt={3} h={"100%"} overflowY={"auto"} className={"hide-scrollbar"}>
         <Container pos={"relative"}>
           <BackRoutButton transform={"translateX(-1rem)"} mb={4} />
-          <Loading isLoading={articlesStore.IsLoading}>
-            {!articlesStore.IsLoading &&
-              (articlesStore.getArticle(parseInt(this.props.id)) ? (
-                <>
-                  <Heading level={1} mb={4}>
-                    {articlesStore.getArticle(parseInt(this.props.id)).title}
-                  </Heading>
-                  <Article
-                    md={articlesStore.getArticle(parseInt(this.props.id)).text}
-                  ></Article>
-                </>
-              ) : (
-                <Text as={"h1"} fontSize={"1rem"} textAlign={"center"}>
-                  Не получилось загрузить статью, или её не существует
-                </Text>
-              ))}
+          <Loading isLoading={this.state.IsLoading}>
+            {this.state.article ? (
+              <>
+                <Heading level={1} mb={4}>
+                  {this.state.article.title}
+                </Heading>
+                <Article md={this.state.article.text}></Article>
+              </>
+            ) : (
+              <Text as={"h1"} fontSize={"1rem"} textAlign={"center"}>
+                Не получилось загрузить статью, или её не существует
+              </Text>
+            )}
           </Loading>
         </Container>
       </VStack>
