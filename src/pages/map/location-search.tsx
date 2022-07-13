2a9f8e5a6983e4d2d6544ad1cda49bcc435428f7
andrey <andrey-ne@mail.ru>
2022-07-13 14:39:07 +0400

    fix location search error

--- src/pages/map/location-search.tsx
+++ src/pages/map/location-search.tsx
@@ -149,9 +149,9 @@ function filterLocations(query: string) {
   const regEx = new RegExp(query, "iu");
   return (location: InsomniaLocationFull) => {
     const simpleResult =
-      location.name.match(regEx) ||
-      location.description.match(regEx) ||
-      location.menu.match(regEx);
+      location.name?.match(regEx) ||
+      location.description?.match(regEx) ||
+      location.menu?.match(regEx);
     if (simpleResult) {
       return true;
     }
@@ -163,9 +163,9 @@ function filterLocations(query: string) {
       .flatMap((x) => x.elements)
       .some(
         (x) =>
-          x.name.match(regEx) ||
-          x.speaker.match(regEx) ||
-          x.description.match(regEx)
+          x.name?.match(regEx) ||
+          x.speaker?.match(regEx) ||
+          x.description?.match(regEx)
       );
   };
 }
