42fb50177948149774c9396a16d4fdb2cf7531f4
Andrei Khamidulin <andrey.khamidulin@tomi.ai>
2023-05-19 20:02:10 +0400

    main page sections

--- src/pages/main/card-list.ts
+++ src/pages/main/card-list.ts
@@ -1,44 +1,185 @@
-export const CardList: Array<CardInfo> = [{
-  id: 'house',
-  title: 'Жилье',
-  color: true,
-  section: Section.about,
-  row: 1,
-  col: 1
-},{
-  id: 'food',
-  title: 'Еда',
-  color: false,
-  section: Section.about,
-  row: 1,
-  col: 2
-},{
-  id: 'water',
-  title: 'Вода',
-  color: false,
-  section: Section.about,
-  row: 1,
-  col: 3
-},
+export const CardList: Array<CardInfo> = [
+  {
+    id: "house",
+    title: "Жилье",
+    color: true,
+    section: Section.about,
+    row: 1,
+    col: 1,
+  },
+  {
+    id: "food",
+    title: "Еда",
+    color: false,
+    section: Section.about,
+    row: 1,
+    col: 2,
+  },
+  {
+    id: "water",
+    title: "Вода",
+    color: false,
+    section: Section.about,
+    row: 1,
+    col: 3,
+  },
 
   {
-    id: 'phone',
-    title: 'Зарядить телефон',
+    id: "phone",
+    title: "Зарядить телефон",
     color: false,
     section: Section.about,
     row: 2,
-    col: 1
+    col: 1,
   },
   {
-    id: 'insight',
-    title: 'Intronet Insomnia',
+    id: "insight",
+    title: "Intronet Insomnia",
     color: true,
     section: Section.about,
     row: 2,
-    col: 2
+    col: 2,
   },
 
-]
+  {
+    id: "child",
+    title: "Я с ребёнком",
+    descr:
+      "Где поселиться, правила безопасности, игровые площадки и активности",
+    color: true,
+    section: Section.about,
+    row: 3,
+    col: 1,
+  },
+
+  {
+    id: "wc",
+    title: "Туалеты и\nдуши",
+    color: false,
+    section: Section.about,
+    row: 4,
+    col: 1,
+  },
+
+  {
+    id: "gc",
+    title: "Мусор",
+    color: false,
+    section: Section.about,
+    row: 4,
+    col: 2,
+  },
+
+  {
+    id: "animation",
+    title: "Анимация",
+    color: true,
+    descr: "Расписание показов, все про мультфильмы в лесу",
+    section: Section.activity,
+    row: 1,
+    col: 1,
+  },
+
+  {
+    id: "music",
+    title: "Музыка",
+    color: false,
+    section: Section.activity,
+    row: 2,
+    col: 1,
+  },
+
+  {
+    id: "voting",
+    title: "Голосование",
+    color: true,
+    section: Section.activity,
+    row: 2,
+    col: 2,
+  },
+
+  {
+    id: "carnaval",
+    title: "Карнавал",
+    color: false,
+    section: Section.activity,
+    row: 3,
+    col: 1,
+  },
+
+  {
+    id: "friends",
+    title: "Найти друзей",
+    color: false,
+    section: Section.activity,
+    row: 3,
+    col: 2,
+  },
+
+  {
+    id: "nonanimation",
+    title: "неАнимация",
+    descr: "Дневные развлечения на фестивале",
+    color: true,
+    section: Section.activity,
+    row: 4,
+    col: 1,
+  },
+
+  {
+    id: "lostChild",
+    title: "Я потерял ребёнка!",
+    descr: "Что делать, куда бежать, где искать...",
+    color: true,
+    section: Section.warning,
+    row: 1,
+    col: 1,
+  },
+
+  {
+    id: "lostStuff",
+    title: "Потерял вещи",
+    color: true,
+    section: Section.warning,
+    row: 2,
+    col: 1,
+  },
+
+  {
+    id: "ants",
+    title: "Клещи",
+    color: false,
+    section: Section.warning,
+    row: 2,
+    col: 1,
+  },
+
+  {
+    id: "medical",
+    title: "Я поранился",
+    descr: "Как добраться до медиков, что делать, чтобы помочь раненому",
+    color: false,
+    section: Section.warning,
+    row: 3,
+    col: 1,
+  },
+  {
+    id: "dogs",
+    title: "Про собак",
+    color: false,
+    section: Section.other,
+    row: 2,
+    col: 1,
+  },
+  {
+    id: "leave",
+    title: "Как уехать",
+    color: true,
+    section: Section.other,
+    row: 2,
+    col: 1,
+  },
+];
 
 export type CardInfo = {
   id: string;
@@ -52,24 +193,28 @@ export type CardInfo = {
   rowSpan?: number;
 };
 
-export const enum Section{
+export const enum Section {
   about,
   activity,
   warning,
-  other
-};
+  other,
+}
 
-export const Sections = [{
-  section: Section.about,
-  title: 'О фестивале'
-},{
-  section: Section.activity,
-  title: 'как тусим'
-},{
-  section: Section.warning,
-  title: 'если чп'
-},{
-  section: Section.other,
-  title: 'Тоже важное'
-}]
-
+export const Sections = [
+  {
+    section: Section.about,
+    title: "О фестивале",
+  },
+  {
+    section: Section.activity,
+    title: "как тусим",
+  },
+  {
+    section: Section.warning,
+    title: "если чп",
+  },
+  {
+    section: Section.other,
+    title: "Тоже важное",
+  },
+];
--- src/pages/main/card/main-card.tsx
+++ src/pages/main/card/main-card.tsx
@@ -1,14 +1,20 @@
-import {FunctionalComponent} from "preact";
-import {Card} from "@components/cards";
-import {CardInfo} from "../card-list";
+import { FunctionalComponent } from "preact";
+import { Card } from "@components/cards";
+import { CardInfo } from "../card-list";
 import styles from "../main-page.module.css";
 
 export type MainCard = {
   info: CardInfo;
-}
-export const MainCard: FunctionalComponent<MainCard> = ({info}) => {
-  return <Card background={info.color ? 'Vivid' : 'White'} flex-grow class={styles.mainCard}>
-    <div>{info.title}</div>
-    <div>{info.descr}</div>
-  </Card>
-}
+};
+export const MainCard: FunctionalComponent<MainCard> = ({ info }) => {
+  return (
+    <Card
+      background={info.color ? "Vivid" : "White"}
+      flex-grow
+      class={info.color ? styles.mainCardColor : styles.mainCard}
+    >
+      <header>{info.title}</header>
+      <div class="textSmall">{info.descr}</div>
+    </Card>
+  );
+};
