5097b09138c137a78158fd848d0d48ad73cd8bfe
mkorolenko <mkorolenko@team.amocrm.com>
2023-05-27 08:51:54 +0300

    удалил лишнюю зависимость

--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -7,13 +7,7 @@ import { MainPage } from "./main/mainPage";
 import { compare } from "@cmmn/cell/lib";
 import { BookmarksPage } from "./bookmarks/bookmarks-page";
 import { VotingPage } from "./voting/voting-page";
-import {
-  StateUpdater,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from "preact/hooks";
+import { StateUpdater, useCallback, useEffect, useState } from "preact/hooks";
 import { OnboardPage } from "./onboard/onboard-page";
 import { NotesPage } from "./notes/NotesPage";
 
