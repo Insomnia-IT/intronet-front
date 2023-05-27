9953b3aa3afe6ea3b3e64fd34f5d6f2ec76f2a35
mkorolenko <mkorolenko@team.amocrm.com>
2023-05-27 09:07:22 +0300

    Небольшой рефактор

--- src/pages/bookmarks/bookmarks-page.tsx
+++ src/pages/bookmarks/bookmarks-page.tsx
@@ -1,12 +1,10 @@
-import { useRouter } from "../routing";
-import { bookmarksStore } from "@stores/bookmarks.store";
-import { useCell } from "@helpers/cell-state";
-import { useCallback, useEffect, useMemo, useState } from "preact/hooks";
 import { CloseButton } from "@components";
-import { Cell } from "@cmmn/cell/lib";
 import { Tag, Tags } from "@components/tag";
+import { useCell } from "@helpers/cell-state";
+import { bookmarksStore } from "@stores/bookmarks.store";
+import { useEffect } from "preact/hooks";
+import { useRouter } from "../routing";
 import { MovieList } from "../timetable/animation/movie-list";
-import { Snackbar } from "./snackbar/snackbar";
 
 export const BookmarksPage = () => {
   const router = useRouter();
@@ -20,8 +18,8 @@ export const BookmarksPage = () => {
   return (
     <div class="page">
       <h1>избранное</h1>
-      <Tags style={{ margin: "16px 0 20px 0" }}>
-        {Sections.map((x) => (
+      <Tags style={{ margin: "16px 0 20px 0" }} tagsList={Sections}>
+        {(x) => (
           <Tag
             key={x}
             value={x}
@@ -30,7 +28,7 @@ export const BookmarksPage = () => {
           >
             {SectionNames[x]}
           </Tag>
-        ))}
+        )}
       </Tags>
       {type == "movie" && <BookmarkMovies />}
       <CloseButton />
