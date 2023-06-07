5f9bc846ed7ae702dd9ff548a37854e2083449a1
mkorolenko <mkorolenko@team.amocrm.com>
2023-06-08 00:32:52 +0300

    Удалил лишние хуки

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,10 +1,6 @@
-export * from "./NoteModal";
 export * from "./LocationModal";
 export * from "./LoginModal";
-export * from "./ScheduleElementModal";
 export * from "./LogoutModal";
-export * from "./CategoriesModal";
-export * from "./CategoryModal";
 
 export type ModalProps<T = any> = {
   show?: boolean;
