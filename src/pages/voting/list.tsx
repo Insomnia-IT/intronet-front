000fb036bfd7bbb534a5a08481807c2716032f53
Andrey Khamidulin <andrey.khamidulin@tomi.ai>
2023-07-05 18:51:04 +0400

    upd ver

--- src/pages/voting/list.tsx
+++ src/pages/voting/list.tsx
@@ -1,14 +1,6 @@
-import {Card} from "@components/cards";
 import { useCell } from "@helpers/cell-state";
-import { votingStore } from "@stores/votingStore";
-import {useEffect, useState} from "preact/hooks";
-import { useRouter } from "../routing";
-import { Input } from "@components/input";
-import { Link } from "@components/link/link";
-import { Button, ButtonsBar } from "@components";
 import { FunctionalComponent } from "preact";
 import {MovieList} from "../timetable/animation/movie-list";
-import { MovieSmall } from "../timetable/animation/movie-small";
 import { moviesStore } from "@stores";
 
 export const VotingList: FunctionalComponent = () => {
