c74f9c25b225477a8d39e0265aaddb6c1599f377
Corvus278 <makar.korolenko2005@mail.ru>
2023-07-05 23:31:18 +0300

    деталка объявления в избранном

--- src/pages/bookmarks/note/BookmarkNotes.tsx
+++ src/pages/bookmarks/note/BookmarkNotes.tsx
@@ -2,12 +2,34 @@ import { BookmarkPlug } from "@components/plugs/bookmark/BookmarkPlug";
 import { useCell } from "@helpers/cell-state";
 import { bookmarksStore } from "@stores/bookmarks.store";
 import { GesturedNotesList } from "../../notes/NotesList/GesturedNoteList";
+import { useState } from "preact/hooks";
+import { Sheet } from "@components";
+import { NoteSheet } from "../../notes/NoteSheet/NoteSheet";
 
 export const BookmarkNotes = () => {
   const notesList = useCell(() => bookmarksStore.Notes);
+  const [activeNoteId, setActiveNoteId] = useState(null);
+  const resetActiveNoteId = () => {
+    setActiveNoteId(null);
+  };
 
   return notesList.length ? (
-    <GesturedNotesList notes={notesList} />
+    <>
+      <GesturedNotesList
+        notes={notesList}
+        notesProps={{
+          onClick: setActiveNoteId,
+        }}
+      />
+      <Sheet onClose={resetActiveNoteId} height="auto">
+        {activeNoteId && (
+          <NoteSheet
+            activeNoteId={activeNoteId}
+            onClose={resetActiveNoteId}
+          ></NoteSheet>
+        )}
+      </Sheet>
+    </>
   ) : (
     <BookmarkPlug
       buttonTitle={"К объявлениям"}
