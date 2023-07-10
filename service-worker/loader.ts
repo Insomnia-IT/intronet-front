12377e80dccbc3f69d570911d245f6a0a92ad242
Andrey Khamidulin <andrey.khamidulin@tomi.ai>
2023-07-10 19:23:45 +0400

    fix

--- service-worker/loader.ts
+++ service-worker/loader.ts
@@ -50,7 +50,7 @@ if (navigator.serviceWorker && !location.href.match("(localhost)")) {
         indexedDB.deleteDatabase('versions');
       })
       .catch()
-      .then(() => location.reload());
+      .then(() => location.pathname = '/');
   }
   if (navigator.serviceWorker.controller) {
     const isIOS = CSS.supports("-webkit-touch-callout", "none");
