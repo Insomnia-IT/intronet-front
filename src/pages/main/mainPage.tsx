49f7267a0ab1e0a6be70937ab92125fdaf038b40
Corvus278 <makar.korolenko2005@mail.ru>
2023-07-11 22:35:09 +0300

    Поправил боковой отсуп главной (на 16px)

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -1,35 +1,34 @@
-import {Sheet} from "@components";
-import {useCell} from "@helpers/cell-state";
+import { Sheet } from "@components";
+import { useCell } from "@helpers/cell-state";
 import { Logo } from "@icons";
-import {mainPageStore} from "@stores/main-page.store";
-import {useMemo} from "preact/hooks";
-import {RoutePath, useRouter} from "../routing";
+import { mainPageStore } from "@stores/main-page.store";
+import { useMemo } from "preact/hooks";
+import { RoutePath, useRouter } from "../routing";
 import styles from "./main-page.module.css";
 import { MainCard } from "./card/main-card";
 import { Menu } from "./menu/menu";
-import {AddNews} from "./news/add-news";
-import {AllNews} from "./news/all-news";
-import {EditNews} from "./news/edit-news";
-import {News} from "./news/news";
+import { AddNews } from "./news/add-news";
+import { AllNews } from "./news/all-news";
+import { EditNews } from "./news/edit-news";
+import { News } from "./news/news";
 
 export const MainPage = () => {
   const state = useCell(mainPageStore.State);
   const router = useRouter();
-  const sheetItems = useMemo(() => getSheetItems(router.route), [router.route])
+  const sheetItems = useMemo(() => getSheetItems(router.route), [router.route]);
   return (
     <div class={styles.container + " dark"}>
-      {/* Login button - кнопка для админов, невидимая, располагается в правом верхнем углу экрана */}
       <div class={styles.mainPage} flex column gap="6">
         <Logo />
-        <News/>
+        <News />
         {state.sections.map((x) => (
           <div key={x.section} flex column gap="2">
             <h2 class="colorWhite">{x.title}</h2>
             <div flex column gap="1">
-              {x.rows.map(({row, cards}) => (
+              {x.rows.map(({ row, cards }) => (
                 <div gap="1" flex>
                   {cards.map((c) => (
-                    <MainCard info={c} key={c}/>
+                    <MainCard info={c} key={c} />
                   ))}
                 </div>
               ))}
@@ -37,23 +36,27 @@ export const MainPage = () => {
           </div>
         ))}
         <Menu />
-        <Sheet children={sheetItems}
-               height={router.route[2] === undefined ? 'auto' : '100%'}
-               onClose={() => router.goTo(["main"])}/>
+        <Sheet
+          children={sheetItems}
+          height={router.route[2] === undefined ? "auto" : "100%"}
+          onClose={() => router.goTo(["main"])}
+        />
       </div>
     </div>
   );
 };
 
-
-function getSheetItems(route: RoutePath){
-  switch (route[1]){
-    case "news": switch (route[2]){
-      case "add": return <AddNews/>;
-      case undefined: return <AllNews/>;
-      default: return <EditNews/>;
-    }
-
+function getSheetItems(route: RoutePath) {
+  switch (route[1]) {
+    case "news":
+      switch (route[2]) {
+        case "add":
+          return <AddNews />;
+        case undefined:
+          return <AllNews />;
+        default:
+          return <EditNews />;
+      }
   }
   return null;
 }
