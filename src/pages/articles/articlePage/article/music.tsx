e9fc291a4654346354d69d71e6998be5fd1a752b
Andrey Khamidulin <andrey.khamidulin@tomi.ai>
2023-07-11 21:10:55 +0400

    fix music timetable

--- src/pages/articles/articlePage/article/music.tsx
+++ src/pages/articles/articlePage/article/music.tsx
@@ -1,20 +1,20 @@
 import {Button, ButtonsBar, CloseButton} from "@components";
-import {Card} from "@components/cards";
 import {Link} from "@components/link/link";
-import {SvgIcon} from "@icons";
+import {locationsStore} from "@stores";
 import {useRouter} from "../../../routing";
 
 export function Music() {
   const router = useRouter();
   if (router.route[2] === 'schedule')
     return <MusicSchedule/>
+  const location = locationsStore.findByName('музыкальная');
   return <div class="page colorMediumBlue" flex gap="4">
     <h1>Музыка</h1>
     <CloseButton goTo="/main"/>
     <div>В лайнапе «Бессонницы» этого года — Краснознаменная дивизия имени моей бабушки, Нотэбёрд 2.0, Secrets of the
       third planet, Samosad bend, ОРК и КО, Vespero, Фёдор Ларюшкин и десятки других групп и музыкантов.
     </div>
-    <Link style={{marginBottom: 8}} goTo="/articles/music/schedule">расписание муз сцена</Link>
+    <Link style={{marginBottom: 8}} goTo={['activities','location',location._id]}>расписание муз сцена</Link>
     <div>Легендарная Бессонничная Карусель снова будет крутиться, звучать, сиять и дымить!</div>
     <Link goTo="/map" query={{name: 'заря'}}>к заре</Link>
   </div>
