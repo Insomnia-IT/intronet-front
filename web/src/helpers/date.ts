d66ef0afe1811720e07a771b7e7e5bd2a327e228
ceylar37 <ceylar37@gmail.com>
2025-06-26 19:40:09 +0300

    feature/change-checkIsNoteActual

--- web/src/helpers/date.ts
+++ web/src/helpers/date.ts
@@ -1,9 +1,7 @@
-/* ! До феста значение замокано на один из дней феста ! */
 export const getCurrentDate = () => {
-  return 13;
-  // const currentDate = new Date().getDate();
-  //
-  // return currentDate > 12 ? currentDate : 14;
+  const currentDate = new Date().getDate();
+
+  return currentDate;
 };
 
 export const getCurrentUtc = () => {
