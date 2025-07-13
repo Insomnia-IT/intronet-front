4c78e43f2b44203238b3313fa2f37928bc482630
svtnester <svt.nester@gmail.com>
2025-07-13 14:57:17 +0300

    fire2

--- web/src/pages/articles/articlePage/article/fire.tsx
+++ web/src/pages/articles/articlePage/article/fire.tsx
@@ -2,23 +2,24 @@ import { CloseButton, Link, PageLayout } from "@components";
 
 export function Fire() {
   return (
-    <PageLayout title="Костры" withCloseButton>
-        <div>
+    <PageLayout title="Костры" withCloseButton gap="4">
+    <CloseButton goTo="/main"/>
+        <div class="text">
           Сейчас в России действуют строгие правила пожарной безопасности. На
           походные газовые горелки в этом законе прямого запрета нет.
         </div>
-        <div>
+        <div class="text">
           Если случилась беда — <b>подходите к волонтёрам с рацией</b>. На
           территории работают службы, которые помогут.
         </div>
-        <div>
+        <div class="text">
           Ещё на поле есть костёр у Фудкорта. Тут нельзя
           готовить, но можно отдохнуть и погреться!
         </div>
-        <Link goTo="/map" query={{ name: "костер" }}>
+        <Link goTo="/map" query={{ name: "костёр" }}>
           к костру у фудкорта
         </Link>
-      <CloseButton goTo="/main"/>
+      
     </PageLayout>
   );
 }
