7016c328f9832409af03f1a5188c778dd0120c9b
svtnester <svt.nester@gmail.com>
2026-07-04 16:19:27 +0300

    Музыканты и Анимаквест2

--- web/src/pages/articles/articlePage/article/music.tsx
+++ web/src/pages/articles/articlePage/article/music.tsx
@@ -8,43 +8,28 @@ export function Music() {
   return (
     <PageLayout title="Музыка" gap={4}>
       <CloseButton goTo="/main" />
-      <div class="sh2">В 2025 году на "Бессоннице" выступят:</div>
+      <div class="sh2">В 2026 году на "Бессоннице" выступят:</div>
       <ul className="disc" style={{ margin: "4px 0" }}>
-        <li>Black lama</li>
-        <li>Krymov</li>
-        <li>Princesse Angine</li>
-        <li>Дом Сутягина</li>
-        <li>Insomnia Improvisers</li>
-        <li>Vespero</li>
-        <li>Yarvi</li>
-        <li>Чародейник</li>
-        <li>Рубашка ft ОзЕрский</li>
-        <li>БОУМ</li>
-        <li>Виктор Скорбенко</li>
-        <li>Конус Маха</li>
-        <li>idst</li>
-        <li>TLFP (the Legendary Flower Punk)</li>
-        <li>Голос Земли</li>
-        <li>Евгений Франкевич</li>
-        <li>Настежь</li>
-        <li>Видеоигры</li>
-        <li>Lesya Lass</li>
-        <li>samosad bend solo</li>
-        <li>Дом прекрасных аустов и Дом престарелых аутистов</li>
-        <li>Usssy</li>
-        <li>миром правят собаки</li>
-        <li>Шаййм</li>
-        <li>Света Матвеева (Деревянные киты)</li>
-        <li>Другое Дело</li>
-        <li>orwell's magazine </li>
-        <li>Secrets of the Third Planet</li>
-        <li>Palobata и друзья</li>
-        <li>Hajime Kojiro + Московская Шумовая Мануфактура </li>
-        <li>Резина</li>
-        <li>мерзавцы</li>
-        <li>Cops on acid</li>
-        <li>ускоритель частиц</li>
-        <li>Айда</li>
+        <li>Краснознаменная дивизия имени моей бабушки</li>
+<li>Black Hole Surfers</li>
+<li>Blednyj</li>
+<li>Insomnia Improvisers Orchestra</li>
+<li>KissLiar</li>
+<li>Mosbrass</li>
+<li>Ordo</li>
+<li>Sadsadsergievposad</li>
+<li>samosad bend solo</li>
+<li>Special guest ОА</li>
+<li>Vermicelli Orchestra</li>
+<li>Видеоигры</li>
+<li>Войка</li>
+<li>Досвидошь</li>
+<li>Нотэбёрд</li>
+<li>Один в оленьем парке</li>
+<li>Талисман</li>
+<li>Теуникова & КоМПОзит</li>
+<li>Топот dark trio</li>
+<li>Шаййм</li>
         <li>... и другие!</li>
       </ul>
 
@@ -53,15 +38,8 @@ export function Music() {
           style={{ marginBottom: 8 }}
           goTo={['activities',{name: 'Музыкальная сцена'}]}
         >
-          Расписание главной музыкальной сцены
+          Расписание музыкальной сцены
         </Link>
-        <Link
-        style={{ marginBottom: 8 }}
-        goTo={['activities',{name: 'Малая сцена'}]}
-      >
-        Расписание малой сцены «Пайтити»
-      </Link>
-
     </PageLayout>
   );
 }
